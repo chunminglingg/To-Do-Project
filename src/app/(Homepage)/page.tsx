0b10dd7374b6c6706d2248667e8125467eb0c228
Chunming <chunming200@gmail.com>
2024-07-25 17:10:43 +0700

    fix &apos;

--- src/app/(Homepage)/page.tsx
+++ src/app/(Homepage)/page.tsx
@@ -17,7 +17,7 @@ const page = () => {
             Feeling overwhelmed? Our easy-to-use Habits tracker help you to
             control of your day and achieve your goals.
           </p>
-          <button className="px-4 py-2.5 bg-indigo-600 text-white font-medium rounded-lg hover:bg-indigo-400 focus:ring-2 focus:ring-indigo-700 shadow-md shadow-indigo-200">Let's get started!</button>
+          <button className="px-4 py-2.5 bg-indigo-600 text-white font-medium rounded-lg hover:bg-indigo-400 focus:ring-2 focus:ring-indigo-700 shadow-md shadow-indigo-200">Let&apos;s get started!</button>
         </div>
       </main>
     </div>
